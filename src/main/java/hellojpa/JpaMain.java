64434865addbaffa81e5dbb3c64227c53e5cb8ad
박희준 <hj.park2@okestro.com>
2025-05-25 23:17:26 +0900

    FEAT. 필드와 컬럼 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -13,47 +13,12 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            // 비영속
-//            Member member = new Member();
-//            member.setId(101L);
-//            member.setName("HelloHJPATest");;
-//
-//            // 영속
-//            System.out.println("=== BEFORE ===");
-//            em.persist(member);
-//            System.out.println("=== AFTER ===");
-//
-//            Member findMember = em.find(Member.class, 101L);
-//            System.out.println("findMember.getId() = " + findMember.getId());
-//            System.out.println("findMember.getName() = " + findMember.getName());
+            Member member = new Member();
+            member.setId(2L);
+            member.setUsername("hjpark");
+            member.setRoleType(RoleType.ADMIN);
 
-//            // 영속
-//            Member findMember1 = em.find(Member.class, 101L);
-//            Member findMember2 = em.find(Member.class, 101L);
-
-//            // 영속
-//            Member member1 = new Member(101L, "Hello");
-//            Member member2 = new Member(102L, "World");
-//
-//            em.persist(member1);
-//            em.persist(member2);
-//
-//            System.out.println("=== AFTER ===");
-
-//            Member member = new Member(200L, "Hello");
-//            em.persist(member);
-//
-//            // 강제 호출
-//            em.flush();
-
-            Member member = em.find(Member.class, 200L);
-            member.setName("World");
-
-            em.clear();
-
-            Member member2 = em.find(Member.class, 200L);
-
-            System.out.println("====================");
+            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,23 +1,41 @@
 package hellojpa;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
-@Table(name = "MBR")
 public class Member {
 
     @Id
     private Long id;
-    private String name;
+
+    @Column(name = "name", nullable = false)
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+
+    @Transient
+    private int temp;
 
     public Member() {
-    }
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
     }
 
     public Long getId() {
@@ -28,11 +46,59 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
     }
 }
