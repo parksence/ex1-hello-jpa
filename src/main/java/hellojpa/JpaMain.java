095ef303403213f265b51e2ee2a2d9fd564204ca
박희준 <hj.park2@okestro.com>
2025-06-09 18:37:21 +0900

    값 타입과 불변 객체

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,13 +14,23 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setName("Hello");
-            member.setHomeAddress(new Address("city", "street", "10000"));
-            member.setWorkPeriod(new Period());
+            Address address = new Address("city", "street", "10000");
 
+            Member member = new Member();
+            member.setName("member1");
+            member.setHomeAddress(address);
             em.persist(member);
-            
+
+            // 값 복사해서 사용하기
+            Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
+
+            Member member2 = new Member();
+            member2.setName("member2");
+            member2.setHomeAddress(copyAddress);
+            em.persist(member2);
+
+            // member.getHomeAddress().setCity("newCity");
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
